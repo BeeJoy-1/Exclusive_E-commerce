47453ed20a188d53d12d212b4b6257b3e943dbf3
BeeJoy-1 <beejoy3000@gmail.com>
2025-01-20 17:48:30 +0600

    did the pagination button part

--- src/components/ProductPageComponents/ProductRight.jsx
+++ src/components/ProductPageComponents/ProductRight.jsx
@@ -10,7 +10,9 @@ const ProductRight = () => {
 
   // Pagination Funtion
   const handlePerItem = (index) => {
-    setpage(index);
+    if (index > 0 && index <= Math.ceil(totalPage)) {
+      setpage(index);
+    }
   };
 
   return (
@@ -43,7 +45,8 @@ const ProductRight = () => {
           <li>
             <span
               href="#"
-              class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              className="flex items-center justify-center px-4 h-10 leading-tight text-text_black000000 bg-white border border-gray-300 cursor-pointer"
+              onClick={() => handlePerItem(page - 1)}
             >
               Previous
             </span>
@@ -52,7 +55,11 @@ const ProductRight = () => {
             <li>
               <span
                 href="#"
-                class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                className={
+                  index + 1 === page
+                    ? "flex items-center justify-center px-4 h-10 leading-tight text-white_FFFFFF bg-text_reddb4444 border border-text_reddb4444"
+                    : "flex items-center justify-center px-4 h-10 leading-tight text-text_black000000 bg-white border border-gray-300 cursor-pointer"
+                }
                 onClick={() => handlePerItem(index + 1)}
               >
                 {index + 1}
@@ -62,7 +69,8 @@ const ProductRight = () => {
           <li>
             <span
               href="#"
-              class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              className="flex items-center justify-center px-4 h-10 leading-tight text-text_black000000 bg-white border border-gray-300 cursor-pointer"
+              onClick={() => handlePerItem(page + 1)}
             >
               Next
             </span>
