2551239aad53f71a214266255b644abf971ca9f6
BeeJoy-1 <beejoy3000@gmail.com>
2025-05-10 13:02:04 +0600

    chat GPT is a blessing in disguise

--- src/components/RootLayout/RootLayout.jsx
+++ src/components/RootLayout/RootLayout.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header/Header";
 import Navbar from "./NavBar/Navbar";
 import Footer from "./Footer/Footer";
 import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getTotal } from "../../Features/AllSlice/ProductSlice";
 
 const RootLayout = () => {
+  const dispatch = useDispatch();
+  const value = useSelector((state) => state.product.value);
+
+  useEffect(() => {
+    dispatch(getTotal());
+  }, [value, dispatch]);
   return (
     <div>
       <Header />
